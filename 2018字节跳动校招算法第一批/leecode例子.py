a1e5f0e8d3bb9a80c4d8715c61710130172b6939
xuyifeng <13588829960@163.com>
2019-08-06 17:52:04 +0800

    添加第2题正确解法和leecode

---
+++ 2018字节跳动校招算法第一批/leecode例子.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 15:52:48 2019
+
+@author: yifeng
+"""
+
+class Solution:
+    def largestRectangleArea(self,heights):
+        heights.append(0)
+        stack = []
+        i = 0
+        result = 0
+        while i<len(heights):
+            if not stack or heights[stack[-1]]<heights[i]:
+                stack.append(i)
+                i += 1      
+            else:
+                num = stack.pop(-1)
+                result = max(result,heights[num]*((i-num) if stack else i))
+            
+        return result
+    
+t = [2,1,5,6,2,3]
+so = Solution()
+so.largestRectangleArea(t)
--- 2018字节跳动校招算法第一批/字节跳动2018校招算法第一批第2题.py
+++ 2018字节跳动校招算法第一批/字节跳动2018校招算法第一批第2题.py
@@ -6,6 +6,56 @@ Created on Mon Aug  5 21:18:06 2019
 """
 
 # 字节跳动2018校招算法第一批第2题
+'''
+给定一个数组序列, 需要求选出一个区间, 使得该区间是所有区间中经过如下计算的值最大的一个：
+
+区间中的最小数 * 区间所有数的和最后程序输出经过计算后的最大值即可，不需要输出具体的区间。
+如给定序列  [6 2 1]则根据上述公式, 可得到所有可以选定各个区间的计算值: 
+[6] = 6 * 6 = 36;
+
+[2] = 2 * 2 = 4;
+
+[1] = 1 * 1 = 1;
+
+[6,2] = 2 * 8 = 16;
+
+[2,1] = 1 * 3 = 3;
+
+[6, 2, 1] = 1 * 9 = 9;
+
+ 
+从上述计算可见选定区间 [6] ，计算值为 36， 则程序输出为 36。
+
+区间内的所有数字都在[0, 100]的范围内;
+'''
+
+##===========方法一==牛逼的解法===============
+
+def main():
+    n = int(input()) 
+    arr = list(map(int,input().split()))
+    stack = []  # 栈
+    arr.append(0) #元素添加0
+    result = 0 # 结果
+    i = 0  #index
+    presum = [] # 本轮第一个元素进栈时，presum.append上一轮元素出栈的和，之后都append 0
+    tempsum = 0 # 本轮的累加
+    while i<len(arr):
+        if not stack or arr[i]>=stack[-1]: # 若栈为空，或arr[i]大于最后一个进栈的元素
+            presum.append(tempsum) # 本轮第一个元素进栈时，presum.append上一轮元素出栈的和，之后都append 0
+            tempsum = 0 
+            stack.append(arr[i])
+            i += 1
+        else:
+            temp = stack.pop(-1) # 出栈
+            tempsum = tempsum + (temp+presum.pop(-1)) # 本轮的累加
+            result = max(result,tempsum*temp)
+    print(result)
+
+main()
+
+#====================方法二===================================================
+# 最初的解法，复杂度过高
 import sys
 
 def main():
@@ -38,4 +88,5 @@ def main():
     print(max_m)
     
 main()
-    
+
+
